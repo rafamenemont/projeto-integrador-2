a6e9c1cff5c65f3fffdaeff85f5c68838279c6e3
RobertSGomes <robertsg.dev@gmail.com>
2022-09-13 19:44:47 -0300

    Feat: Criando Landing Page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import LandingPage from "./views/LandingPage/LadingPage";
 import PageNotFound from "./views/PageNotFoiund/PageNotFoiund"
 
 const AppRoutes = () => {
 
   return (
     <div>
-          <Routes>
-            <Route path={'/'} element={<PageNotFound />} />
-            {/* <PrivateRoutes path={'/search'} element={<PageNotFound />} /> */}
-            {/* <MenuAndHeader> */}
-            {/* <PrivateRoutes path={'/inicial'} element={<PageNotFound />} /> */}
-            {/* </MenuAndHeader> */}
-            <Route path={'*'} element={<PageNotFound />} />
-          </Routes>
+      <Routes>
+        <Route path={'/'} element={<LandingPage />} />
+        {/* <PrivateRoutes path={'/search'} element={<PageNotFound />} /> */}
+        {/* <MenuAndHeader> */}
+        {/* <PrivateRoutes path={'/inicial'} element={<PageNotFound />} /> */}
+        {/* </MenuAndHeader> */}
+        <Route path={'*'} element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 };
