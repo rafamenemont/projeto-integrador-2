c9a6911ca74040a2ae0ce093e9f1901646c6b2b9
RobertSGomes <robertsg.dev@gmail.com>
2022-09-22 16:07:57 -0300

    Feat: Select option finalizado

--- src/views/Dashboard/Modal/index.tsx
+++ src/views/Dashboard/Modal/index.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react"
+import Select from 'react-select'
 import { SButton, TButton } from "../../../Components/Button/button"
 import { Input } from "../../../Components/Input/input"
 import { Fetch } from "../../../modules/fetch";
-import { GroupBtn, IconCancel, IconClose, IconConfirm, IconPay, IconReceive, IconTrash, IconWallet, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalRow, ModalShadow } from "./style"
+import { GroupBtn, GroupSelect, IconCancel, IconClose, IconConfirm, IconPay, IconReceive, IconTrash, IconWallet, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalRow, ModalShadow } from "./style"
 
 interface StyledModalProps {
     onClick: () => void;
@@ -44,34 +45,88 @@ export const ModalNewPay: React.FC<StyledModalProps> = ({ onClick }) => {
     const [adress, setAdress] = useState<string>('')
     const [payment, setPayment] = useState<string>('')
 
-    function handleNewPay() {
-        setNewRec(!newRec)
-        setNewPay(!newPay)
+    const originOptions = [
+        { value: 'Nacional', label: 'Nacional' },
+        { value: 'Internacional', label: 'Internacional' }
+    ]
+
+    const paymentOptions = [
+        { value: 'Dinheiro', label: 'Dinheiro' },
+        { value: 'Cartão de crédito', label: 'Cartão de crédito' },
+        { value: 'Cartão de débito', label: 'Cartão de débito' },
+        { value: 'PIX', label: 'PIX' },
+        { value: 'Outro', label: 'Outro' }
+    ]
+
+    const selectStyles = {
+        option: (provided: any, state: any) => ({
+            ...provided,
+            color: state.isSelected ? 'white' : 'rgba(255,255,255,0.5)',
+            padding: 20,
+            background: state.isSelected ? '#6D65D3' : '#1F212A',
+            cursor: 'pointer'
+        }),
+        control: () => ({}),
+        singleValue: (provided: any, state: any) => {
+            const opacity = state.isDisabled ? 0.5 : 1;
+            const transition = 'opacity 300ms';
+            const color = 'white';
+
+            return { ...provided, opacity, transition, color };
+        },
+        indicatorsContainer: () => ({
+            display: 'none'
+        }),
+        input: () => ({
+            height: '0px',
+            fontSize: '0px'
+        }),
+        valueContainer: () => ({
+            background: '#1F212A',
+            borderRadius: '10px',
+            padding: '15px',
+            cursor: 'pointer'
+        }),
+        menu: (provided: any) => ({
+            ...provided,
+            background: '#1F212A',
+            borderRadius: '10px'
+        }),
+        menuList: (provided: any) => ({
+            ...provided,
+            background: '#1F212A',
+            borderRadius: '10px'
+        })
     }
 
     const handleChangeDate = (event: React.FormEvent<HTMLInputElement>) => {
         setDate(event.currentTarget.value)
-    };
+    }
 
     const handleChangeCost = (event: React.FormEvent<HTMLInputElement>) => {
         setCost(event.currentTarget.value)
-    };
+    }
 
-    const handleChangeOrigin = (event: React.FormEvent<HTMLInputElement>) => {
-        setOrigin(event.currentTarget.value)
-    };
+    const handleChangeOrigin = (event: any) => {
+        setOrigin(event.value)
+    }
 
     const handleChangeDescription = (event: React.FormEvent<HTMLInputElement>) => {
         setDescription(event.currentTarget.value)
-    };
+    }
 
     const handleChangeAdress = (event: React.FormEvent<HTMLInputElement>) => {
         setAdress(event.currentTarget.value)
-    };
+    }
 
-    const handleChangePayment = (event: React.FormEvent<HTMLInputElement>) => {
-        setPayment(event.currentTarget.value)
-    };
+    const handleChangePayment = (event: any) => {
+        setPayment(event.value)
+    }
+
+    function handleNewPay() {
+        setNewRec(!newRec)
+        setNewPay(!newPay)
+    }
 
     async function handleNewTransaction() {
         const fetchClass = new Fetch<INewTransaction>("Transaction/insert.php");
@@ -108,10 +163,16 @@ export const ModalNewPay: React.FC<StyledModalProps> = ({ onClick }) => {
                 <ModalBody>
                     <Input onChange={handleChangeDate} type="date" placeholder="Data da transação" label="Data" />
                     <Input onChange={handleChangeCost} type="text" placeholder="R$ 99,99" label={newPay ? "Gasto" : "A receber"} />
-                    <Input onChange={handleChangeOrigin} type="text" placeholder="Nacional / Internacional" label="Origem" />
+                    <GroupSelect>
+                        <label>Origem</label>
+                        <Select placeholder="Selecione a origem..." styles={selectStyles} onChange={handleChangeOrigin} options={originOptions} />
+                    </GroupSelect>
                     <Input onChange={handleChangeDescription} type="text" placeholder={newPay ? "Farmácia" : "Salário"} label="Tipo" />
                     <Input onChange={handleChangeAdress} type="text" placeholder="Endereço" label="Endereço" />
-                    <Input onChange={handleChangePayment} type="text" placeholder="Cartão de crédito" label="Forma de pagamento" />
+                    <GroupSelect>
+                        <label>Forma de pagamento</label>
+                        <Select placeholder="Selecione a forma de pagamento..." styles={selectStyles} onChange={handleChangePayment} options={paymentOptions} />
+                    </GroupSelect>
                 </ModalBody>
                 <ModalFooter>
                     <GroupBtn>
@@ -134,27 +195,81 @@ export const ModalAlterPay: React.FC<StyledModalProps> = ({ onClick }) => {
 
     const handleChangeDate = (event: React.FormEvent<HTMLInputElement>) => {
         setDate(event.currentTarget.value)
-    };
+    }
 
     const handleChangeCost = (event: React.FormEvent<HTMLInputElement>) => {
         setCost(event.currentTarget.value)
-    };
+    }
 
-    const handleChangeOrigin = (event: React.FormEvent<HTMLInputElement>) => {
-        setOrigin(event.currentTarget.value)
-    };
+    const handleChangeOrigin = (event: any) => {
+        setOrigin(event.value)
+    }
 
     const handleChangeDescription = (event: React.FormEvent<HTMLInputElement>) => {
         setDescription(event.currentTarget.value)
-    };
+    }
 
     const handleChangeAdress = (event: React.FormEvent<HTMLInputElement>) => {
         setAdress(event.currentTarget.value)
-    };
+    }
 
-    const handleChangePayment = (event: React.FormEvent<HTMLInputElement>) => {
-        setPayment(event.currentTarget.value)
-    };
+    const handleChangePayment = (event: any) => {
+        setPayment(event.value)
+    }
+
+    const originOptions = [
+        { value: 'Nacional', label: 'Nacional' },
+        { value: 'Internacional', label: 'Internacional' }
+    ]
+
+    const paymentOptions = [
+        { value: 'Dinheiro', label: 'Dinheiro' },
+        { value: 'Cartão de crédito', label: 'Cartão de crédito' },
+        { value: 'Cartão de débito', label: 'Cartão de débito' },
+        { value: 'PIX', label: 'PIX' },
+        { value: 'Outro', label: 'Outro' }
+    ]
+
+    const selectStyles = {
+        option: (provided: any, state: any) => ({
+            ...provided,
+            color: state.isSelected ? 'white' : 'rgba(255,255,255,0.5)',
+            padding: 20,
+            background: state.isSelected ? '#6D65D3' : '#1F212A',
+            cursor: 'pointer',
+        }),
+        control: () => ({}),
+        singleValue: (provided: any, state: any) => {
+            const opacity = state.isDisabled ? 0.5 : 1;
+            const transition = 'opacity 300ms';
+            const color = 'white';
+
+            return { ...provided, opacity, transition, color };
+        },
+        indicatorsContainer: () => ({
+            display: 'none'
+        }),
+        input: () => ({
+            height: '0px',
+            fontSize: '0px'
+        }),
+        valueContainer: () => ({
+            background: '#1F212A',
+            borderRadius: '10px',
+            padding: '15px',
+            cursor: 'pointer'
+        }),
+        menu: (provided: any) => ({
+            ...provided,
+            background: '#1F212A',
+            borderRadius: '10px'
+        }),
+        menuList: (provided: any) => ({
+            ...provided,
+            background: '#1F212A',
+            borderRadius: '10px'
+        })
+    }
 
     async function handleAlterTransaction() {
         const fetchClass = new Fetch<IAlterTransaction>("Transaction/update.php");
@@ -194,10 +309,16 @@ export const ModalAlterPay: React.FC<StyledModalProps> = ({ onClick }) => {
                 <ModalBody>
                     <Input onChange={handleChangeDate} type="date" placeholder="Data da transação" label="Data" />
                     <Input onChange={handleChangeCost} type="text" placeholder="R$ 99,99" label="Valor" />
-                    <Input onChange={handleChangeOrigin} type="text" placeholder="Nacional / Internacional" label="Origem" />
+                    <GroupSelect>
+                        <label>Origem</label>
+                        <Select placeholder="Selecione a origem..." styles={selectStyles} onChange={handleChangeOrigin} options={originOptions} />
+                    </GroupSelect>
                     <Input onChange={handleChangeDescription} type="text" placeholder="Gasto com farmácia" label="Tipo" />
                     <Input onChange={handleChangeAdress} type="text" placeholder="Endereço" label="Endereço" />
-                    <Input onChange={handleChangePayment} type="text" placeholder="Cartão de crédito" label="Forma de pagamento" />
+                    <GroupSelect>
+                        <label>Forma de pagamento</label>
+                        <Select placeholder="Selecione a forma de pagamento..." styles={selectStyles} onChange={handleChangePayment} options={paymentOptions} />
+                    </GroupSelect>
                 </ModalBody>
                 <ModalFooter>
                     <GroupBtn>
